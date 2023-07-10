2c78c24ff99acc3b296babb831abae7c1c9cbf1f
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-07-10 20:56:34 -0300

    Nuevos cambios

--- InterfacesEjemplos/Program.cs
+++ InterfacesEjemplos/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Usos de Interfaces
+
+
+
+var persona = new Persona();
+persona.PreguntarNombre();
+Console.WriteLine(persona.Nombre);
+public class Persona : IPersona {
+
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public int Edad { get; set; }
+    public bool EnActivo { get; set; }
+    public DateOnly FechaNacimiento { get; set; }
+
+    public void PreguntarNombre() {
+
+        Console.WriteLine("Cual es tu Nombre?");
+        var nombre = Console.ReadLine();
+        if (nombre == null)
+        {
+            //
+        }
+        else { 
+        Nombre = nombre;
+        }
+    }
+
+
+}
+public interface IPersona {
+
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public int Edad { get; set; }
+    public bool EnActivo { get; set; }
+    public DateOnly FechaNacimiento { get; set; }
+
+    public void PreguntarNombre();
+}
