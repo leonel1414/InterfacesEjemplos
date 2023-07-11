7dc8fa879fa3fd8a26b37b57c5a1be4cdd48a296
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-07-10 21:23:25 -0300

    Modificaciones

--- InterfacesEjemplos/Program.cs
+++ InterfacesEjemplos/Program.cs
@@ -1,10 +1,8 @@
 ﻿//Usos de Interfaces
 
-
-
 var persona = new Persona();
-persona.PreguntarNombre();
-Console.WriteLine(persona.Nombre);
+//persona.PreguntarNombre();
+//Console.WriteLine(persona.Nombre);
 public class Persona : IPersona {
 
     public string Nombre { get; set; }
@@ -13,19 +11,33 @@ public class Persona : IPersona {
     public bool EnActivo { get; set; }
     public DateOnly FechaNacimiento { get; set; }
 
-    public void PreguntarNombre() {
+
+    public Persona() {
+        PreguntarDatos();
+        EscribirDatos();
+    }
+
+    public void PreguntarDatos() {
 
         Console.WriteLine("Cual es tu Nombre?");
         var nombre = Console.ReadLine();
         if (nombre == null)
         {
-            //
+            // mensaje que no introdujo el nombre
         }
         else { 
         Nombre = nombre;
         }
+
+        Console.WriteLine("Cual es tu Fecha de nacimiento (aaaa/mm/dd)?");
+        var fecha = Console.ReadLine();
+        FechaNacimiento = DateOnly.Parse(fecha);
     }
 
+    public void EscribirDatos() {
+    
+    Console.WriteLine($"Nombre:{Nombre}, Fecha de Nacimiento:{FechaNacimiento}");
+    }
 
 }
 public interface IPersona {
@@ -36,5 +48,6 @@ public interface IPersona {
     public bool EnActivo { get; set; }
     public DateOnly FechaNacimiento { get; set; }
 
-    public void PreguntarNombre();
+    public void PreguntarDatos();
+    public void EscribirDatos() { }
 }
